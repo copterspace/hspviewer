8bdbaf9b0bd07b3ce8fef3bfc9d6c736090c2463
RusParinger <63102737+RusParinger@users.noreply.github.com>
2021-01-20 12:03:36 +0400

    Distance computation according to selected layers
    + "diag_flags" init changed

--- hyperspecter_viewver.py
+++ hyperspecter_viewver.py
@@ -8,7 +8,8 @@ images_ext_list = ['.jpg', '.jpeg', '.png', '.bmp', '.tif', 'RGB Image'] #Images
 
 def run_euc(matrix_a, matrix_b):
     global diag_flags
-    dist = numpy.sqrt(numpy.sum((numpy.transpose(matrix_a, (1, 2, 0)) - matrix_b)**2, axis=2))
+    selections_list = [i for i in range(len(diag_flags)) if diag_flags[i]]
+    dist = numpy.sqrt(numpy.sum((numpy.transpose(matrix_a, (1, 2, 0))[:, :, selections_list] - matrix_b[selections_list])**2, axis=2))
     temp_min = numpy.min(dist)
     return 1.0 - (dist - temp_min) / (numpy.max(dist) - temp_min)
 
@@ -83,8 +84,9 @@ def onmouse(event, x, y, flags, param):
 
 def onmouse_diagram(event, x, y, flags, param):
     global num_layers, current_layer, diag_data, diag_flags
-    if type(diag_flags) is not numpy.ndarray:
-        diag_flags=numpy.ones_like(diag_data, dtype=int)
+    #if type(diag_flags) is not numpy.ndarray:
+    #    diag_flags=numpy.ones_like(diag_data, dtype=int)
+    #    print(diag_flags.shape)
 
     if (event == cv2.EVENT_LBUTTONUP):
         col_width = min(1980//len(diag_data), 15)
@@ -117,6 +119,7 @@ def create_new_pipeline():
             img = cv2.imdecode(numpy.frombuffer(f.read(), dtype=numpy.uint8), cv2.IMREAD_COLOR)
             hypercube = img.transpose((2, 0, 1))
         num_layers = hypercube.shape[0]
+        print(num_layers)
         cv2.namedWindow( "Image" )
         cv2.namedWindow( "Settings", cv2.WINDOW_NORMAL )
         cv2.resizeWindow("Settings", 300, 100)
@@ -125,7 +128,7 @@ def create_new_pipeline():
         cv2.createTrackbar('red_edge', 'Settings', num_layers//2, num_layers-1, OnRedEdgeChange)
         cv2.setTrackbarMin('red_edge', 'Settings', 1)
         current_layer = 0
-        diag_flags = None
+        diag_flags = numpy.ones((num_layers,), dtype=int)
         OnLayerChange(0)
 
 def save_diag_data():
